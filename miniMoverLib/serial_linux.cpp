c88a81a16e214a0e36c2efcf47a8fa8f2c499b9a
David Tucker <david.tucker@goliathindustries.com>
2018-10-03 21:16:37 -0700

    Revert "Initial stab at x64 support, need to clean up file i/o still"
    This reverts commit a3f69932f1158085885d8487f9593fb23b10d9ac.

--- miniMoverLib/serial_linux.cpp
+++ miniMoverLib/serial_linux.cpp
@@ -33,7 +33,7 @@
 
 int SerialHelper::m_portCount = 0;
 int SerialHelper::m_defaultPortID = -1;
-SerialHelper::PortInfo SerialHelper::portInfo[SerialHelper::m_maxPortCount] = {"",""};
+SerialHelper::PortInfo SerialHelper::portInfo[SerialHelper::m_maxPortCount] = {0};
 
 int SerialHelper::queryForPorts(const char *hint)
 {
